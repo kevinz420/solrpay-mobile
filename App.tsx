e407f23430efe0be2bac00052cb5dce81017896a
kevinz420 <kzhang0305@gmail.com>
2022-08-12 16:37:40 -0600

    connect/dc

--- App.tsx
+++ App.tsx
@@ -1,28 +1,38 @@
 import React, {type PropsWithChildren} from 'react';
 import {Provider as ReduxProvider} from 'react-redux';
 import store from './redux/app/store';
-import { useAppSelector } from "./redux/app/hooks";
+import {useAppSelector} from './redux/app/hooks';
 import {NativeRouter, Route, Link} from 'react-router-native';
 import {ApplicationProvider} from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
 
 import {TailwindProvider} from 'tailwindcss-react-native';
 import {Header} from './components/Header';
+import {clusterApiUrl} from '@solana/web3.js';
+import {ConnectionProvider} from '@solana/wallet-adapter-react';
+import ConnectButton from './components/ConnectButton';
+import DisconnectButton from './components/DisconnectButton';
+
+const DEVNET_ENDPOINT = /*#__PURE__*/ clusterApiUrl('devnet');
 
 const App = () => {
-  const user = useAppSelector((state) => state.user);
+  const user = useAppSelector(state => state.user);
 
   return (
-    <TailwindProvider>
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <ReduxProvider store={store}>
-          <NativeRouter>
-            <Header />
-            {/* <Route path="/" element={} /> */}
-          </NativeRouter>
-        </ReduxProvider>
-      </ApplicationProvider>
-    </TailwindProvider>
+    <ConnectionProvider endpoint={DEVNET_ENDPOINT}>
+      <TailwindProvider>
+        <ApplicationProvider {...eva} theme={eva.light}>
+          <ReduxProvider store={store}>
+            <NativeRouter>
+              <Header />
+              <ConnectButton color="primary"/>
+              <DisconnectButton color="primary"/>
+              {/* <Route path="/" element={} /> */}
+            </NativeRouter>
+          </ReduxProvider>
+        </ApplicationProvider>
+      </TailwindProvider>
+    </ConnectionProvider>
   );
 };
 
